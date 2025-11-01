af130656578120cf5edb2677bf95c759cd4512e9
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2025-11-01 13:36:49 +0530

    Update Stack.java

--- Stack.java
+++ Stack.java
@@ -1,3 +1,17 @@
-class Stack:
+class Stack<T> {
+    T value;
+
+    void push(T value) {
+    
+  
+
+public class Main {
+    public static void main(String[] args) {
+        Stack<T> myStack = new Stack<>();
+        myStack.push(2);
+    
+    
+  
+
   
   
